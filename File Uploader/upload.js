59335f2f57db3ce966bc3212fd1965221889da17
vaibhav-wappnet <vaibhavw.wappnet@gmail.com>
2023-02-16 16:08:53 +0530

    final commit

--- File Uploader/upload.js
+++ File Uploader/upload.js
@@ -1,29 +1,35 @@
-
 console.log("script lag gai bhai!");
-// showAddFiles();
 
 let getFile = document.getElementById("upload");
-// console.log(getFile);
-
 let addBtn = document.getElementById("btnAdd");
+let fileText = document.getElementById("fileText");
+// console.log(fileText.value);
+// console.log(getFile);
 
 addBtn.addEventListener("click", (e) => {
 
     e.preventDefault();
 
     let files = localStorage.getItem("files");
+    // console.log(files);
 
-    getFile.value == 0 ? alert("Please select a file") : alert(`${getFile.value} File added successfully!`);
+    if (getFile.value == 0)
+        return alert("Please select a file")
+    else if (fileText.value == 0) {
+        return alert("Please Enter the file name!")
+    }
     files === null ? filesObj = [] : filesObj = JSON.parse(files);
-    filesObj.push(getFile.value)
+    filesObj.push(getFile.value && fileText.value);
 
     localStorage.setItem("files", JSON.stringify(filesObj));
     getFile.value = null;
-    console.log(getFile);
-    console.log(filesObj);
+    fileText.value = "";
+
+    // console.log(getFile);
+    // console.log(filesObj);
     // console.log(localStorage);
 
-    // showAddFiles();
+    showAddFiles();
 });
 
 
@@ -34,31 +40,71 @@ const showAddFiles = () => {
 
     let html = "";
 
-    filesObj.forEach((file, index) => {
+    filesObj.map((file, index) => {
+
+        // console.log(file);
+
         html += `
             <div class="added-files">
                 <ul>
                     <li>${index + 1}. ${file}</li>
                 </ul>
-            </div>
-            <div class="buttons">
-                <button class="btn" id="delete">delete</button>
-                <button class="btn"id="re-upload">upload again</button>
+                <button class="btn" id="delete" onclick="deleteFiles(this.id)">delete</button>
             </div>`;
     });
 
     let fileElm = document.getElementById("files");
-    // filesObj.length != 0 ? fileElm.innerHTML = html : fileElm.innerHTML = `No files uploaded here. <br> Use <b> add files </b> section to upload files`;
 
-    // if (filesObj.length != 0) {
-    //     fileElm.innerHTML = html;
-    // }
-    // else {
-    //     fileElm.innerHTML = `No files uploaded here. <br> Use <b> add files </b> section to upload files`;
-    // }
-    fileElm.innerHTML = html;
+    // console.log('fileElm', fileElm)
+
+    filesObj.length != 0 ? fileElm.innerHTML = html : fileElm.innerHTML = `<p>No files uploaded here. <br> Use <b> add files </b> section to upload files</p>`;
 }
 
+// delete files
+
+let deleteFiles = (index) => {
+    let files = localStorage.getItem("files");
+    files === null ? filesObj = [] : filesObj = JSON.parse(files);
+
+    filesObj.splice(index, 1);
+    localStorage.setItem("files", JSON.stringify(filesObj));
+
+    // console.log(localStorage);
+
+    showAddFiles();
+}
+
+
+
+// let uploadAgain = () => {
+
+//     addBtn.addEventListener("click", (e) => {
+        
+//         e.preventDefault();
+        
+//         let files = localStorage.getItem("files");
+//         // console.log(files);
+        
+//         if (getFile.value == 0)
+//         return alert("Please select a file")
+//         else if (fileText.value == 0) {
+//             return alert("Please Enter the file name!")
+//         }
+//         files === null ? filesObj = [] : filesObj = JSON.parse(files);
+//         filesObj.push(getFile.value && fileText.value);
+        
+//         localStorage.setItem("files", JSON.stringify(filesObj));
+//         getFile.value = null;
+//         fileText.value = "";
+        
+//         // console.log(getFile);
+//         // console.log(filesObj);
+//         // console.log(localStorage);
+        
+//         showAddFiles();
+//     });
+//     deleteFiles();
+//     showAddFiles();
+// }
+
 showAddFiles();
-
-
